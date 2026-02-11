e10f44ef7f366721e7a3dc827f217c49879a3b14
felisosa <felipesosabianciotto@gmail.com>
2026-02-11 12:03:39 -0300

    findAll y add de partidos

--- src/partido/partido.controler.ts
+++ src/partido/partido.controler.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from "express"
-import { Partido } from "./partidos.js"
+import {Partido} from './partido.entity.js'
+import {orm} from '../shared/db/orm.js'
+
+const em = orm.em
 
 function sanitizePartidoInput(req: Request, res: Response, next: NextFunction){
     req.body.sanitizedInput={    
@@ -7,8 +10,11 @@ function sanitizePartidoInput(req: Request, res: Response, next: NextFunction){
         tipo: req.body.tipo,
         horario: req.body.horario,
         lugar: req.body.lugar,
-        rival: req.body.rival,
+        rival: req.body.rival, // puedo poner rivales q no esten cargados. Esta bien?
         nroFecha: req.body.nroFecha,
+        equipo: req.body.equipo, //este equipo es al q se le carga el partido lo puse pq esta en la relacion
+                                    // lo puse pq esta en la relacion de equipo a partido pq est mal cargar un partido sin equipo, pero no se si es asi como se hace o si hay q cargar el partido y despues asignarle el equipo, lo puse asi pq es mas facil cargarlo todo junto pero no se si es asi como se hace o si hay q cargar el partido y despues asignarle el equipo, lo puse asi pq es mas facil cargarlo todo junto pero no se si es asi como se hace o si hay q cargar el partido y despues asignarle el equipo
+                                    // ver dsp q hacer en los casos de finales, semis y eso q tdv no se saben los eqipos
     }
 
     Object.keys(req.body.sanitizedInput).
@@ -20,14 +26,27 @@ function sanitizePartidoInput(req: Request, res: Response, next: NextFunction){
 }
 
 async function findAll(req:Request, res:Response) {
-    res.status(500).json({message: 'Not implemented'})
+    try{
+        const partidos = await em.find(Partido, {}, {populate: ['equipo']})  // en el popultate se pone las entidades relacionadas q se quieren traer
+        res.status(200).json({message: "found all teams", data:partidos})
+    } catch (error:any){
+        res.status(500).json({message: error.message})
+}
 }
 async function findOne(req:Request,res:Response) {
     res.status(500).json({message: 'Not implemented'})
 }  
 
 async function add(req:Request, res:Response) {
-  res.status(500).json({message: 'Not implemented'})
+    try{
+        const partido=em.create(Partido, req.body.sanitizedInput)
+        await em.flush()
+        res.status(201).json({message: 'Partido created', data: partido})
+    }
+    catch (error:any){
+        res.status(500).json({message: error.message})
+    }
+  
 }
 
 async function update (req:Request, res:Response) {
@@ -38,5 +57,6 @@ async function update (req:Request, res:Response) {
 async function remove(req:Request, res:Response){
     res.status(500).json({message: 'Not implemented'})
     }
+
 
 export { sanitizePartidoInput, findAll, findOne, add, update, remove }
